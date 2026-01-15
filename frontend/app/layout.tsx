97f6e754277c7f5e901606c9cf237ac368d360e0
gabito1451 <obiagaziegabriel@gmail.com>
2026-01-15 05:09:18 -0800

    Initial Next.js setup

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Stellarts - Uber for Artisans | Built on Stellar Blockchain',
+  icons: {
+    icon: "/Stellarts.png",
+  },
+  description:
+    'Connect with trusted artisans in your area. Stellarts is a decentralized marketplace platform leveraging Stellar blockchain for secure, transparent, and fast transactions.',
+  openGraph: {
+    images: [
+      {
+        url: '',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    images: [
+      {
+        url: '',
+      },
+    ],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
